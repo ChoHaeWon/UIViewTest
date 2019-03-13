9956207ecd1c61558738523768bdf8a35ab2c572
D7703_15 <d7703_15@d7703-15ui-imac.local>
2019-03-13 15:32:30 +0900

    201551079조해원

--- ButtonFun01/ViewController.swift
+++ ButtonFun01/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var mySubView: UIView!
+    var checked=false;
+    //클릭안했을때 false, 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.yellow
+        
+        print(Label.text!)
+        Label.text = "Hello iPhone!!"
+        
+        mySubView.backgroundColor = UIColor.red
     }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if(checked == false){
+            mySubView.backgroundColor = UIColor.blue;
+            checked = true;
+        }
+        else if(checked == true){
+            mySubView.backgroundColor = UIColor.red;
+            checked = false;
+        }
+    }
+    
 }
 
